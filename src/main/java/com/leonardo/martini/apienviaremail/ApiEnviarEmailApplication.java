cb9c265b8c77eb3552b755fb3cba9bab07d5d041
Leonardo Lemos Martini <leonardo.martini@cwi.com.br>
2023-10-02 13:09:36 -0300

    Adicionado estrutura inicial da api

---
+++ src/main/java/com/leonardo/martini/apienviaremail/ApiEnviarEmailApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.martini.apienviaremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEnviarEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEnviarEmailApplication.class, args);
+	}
+
+}
